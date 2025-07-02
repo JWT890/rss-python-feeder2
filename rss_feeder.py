f1b0519bb0e85084fdba3703740cf465a632305f
JWT890 <jwtaylor659@gmail.com>
2025-07-02 15:38:44 -0500

    added rss feeds

---
+++ rss_feeder.py
@@ -0,0 +1,31 @@
+import feedparser
+from datetime import datetime
+
+RSS_FEEDS = {
+    'Risky Business': "http://risky.biz/feeds/risky-business-news/",
+    'ESPN': "https://www.espn.com/espn/rss/news",
+    'HNRSS': "https://hnrss.org/frontpage",
+    'SANS Daily': "https://isc.sans.edu/dailypodcast.xml",
+    'SANS Education': "https://isc.sans.edu/rssfeed.xml",
+    'Microsoft Blog': "https://msrc.microsoft.com/blog/categories/security-research-defense/feed",
+    'Darknet Diaries': "https://www.darknet.org.uk/feed/",
+    'Krebs on Security': "https://krebsonsecurity.com/feed/",
+    'Security Magazine': "https://www.securitymagazine.com/rss/15",
+    'CISA News': "https://www.cisa.gov/news.xml",
+    'CISA Blog': "https://www.cisa.gov/blog.xml",
+    'CISA Cyber Advisory': "https://www.cisa.gov/cybersecurity-advisories/all.xml",
+    'NY Times': "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "NPR News": "https://feeds.npr.org/1001/rss.xml",
+    'NPR Technology': "https://feeds.npr.org/1019/rss.xml",
+    "Simplecast": "https://feeds.simplecast.com/54nAGcIl",
+    "The Record": "https://therecord.media/tag/rss",
+    "Ars Technica": "https://feeds.arstechnica.com/arstechnica/index",
+    "TedTalks": "http://feeds.feedburner.com/TEDTalks_video",
+    "r/technology": "https://www.reddit.com/r/technology/.rss",
+    "BBC News": "http://feeds.bbci.co.uk/news/world/rss.xml",
+    "New Heights": "https://rss.art19.com/new-heights",
+    "Politico": "https://www.politico.com/rss/politicopicks.xml",
+    "Politico Cybersecurity": "https://rss.politico.com/morningcybersecurity.xml",
+    "CISO Series": 'https://cisoseries.libsyn.com/rss',
+    'CS Hub': 'https://www.cshub.com/rss/articles',
+}
